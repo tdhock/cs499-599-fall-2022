b2e355d6f9ac6382cd6e148afe984397c3862865
Toby Dylan Hocking <tdhock5@gmail.com>
2022-10-07 10:01:32 -0700

    two for loops

--- demos/06_torch_linear_model.py
+++ demos/06_torch_linear_model.py
@@ -32,13 +32,37 @@ for set_name, is_set in is_set_dict.items():
     set_labels[set_name] = spam_labels[is_set]
 {set_name:array.shape for set_name, array in set_features.items()}
 
+class CSV(torch.utils.data.Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+    def __getitem__(self, item):
+        return self.features[item,:], self.labels[item]
+    def __len__(self):
+        return len(self.labels)
+ds = CSV(set_features["subtrain"], set_labels["subtrain"])
+ds[0]
+dl = torch.utils.data.DataLoader(
+    ds, batch_size=2, shuffle=True)
+for batch_features, batch_labels in dl:
+    pass
+batch_features
+batch_features.shape
+batch_labels
+
 import torch
 class LinearModel(torch.nn.Module):
     def __init__(self, num_inputs):
         super(LinearModel, self).__init__()
         self.weight_vec = torch.nn.Linear(num_inputs, 1)
+    def getitem(self, item):
+        weights, intercept = [p for p in self.weight_vec.parameters()]
+        return weights.data[0][item]
     def forward(self, feature_mat):
         return self.weight_vec(feature_mat)
+LM=LinearModel(10)
+LM.getitem(5)
+
 
 class Net(torch.nn.Module):
     def __init__(self, *units_per_layer):
@@ -55,15 +79,18 @@ class Net(torch.nn.Module):
     def forward(self, feature_mat):
         return self.stack(feature_mat)
 
+nrow, ncol = set_features["subtrain"].shape
+net = Net(ncol, 100, 10, 1)
+net(batch_features.float())
+
 sizes_dict = {
-    "linear":(ncol, 1),
-    "nnet":(ncol, 100, 10, 1)
+    "linear":((ncol, 1), 0.1),
+    "nnet":((ncol, 100, 10, 1), 0.01),
     }
 model_dict = {}
-for model_name, units_per_layer in sizes_dict.items():
+for model_name, (units_per_layer, step_size) in sizes_dict.items():
     model_dict[model_name] = Net(*units_per_layer)
 
-nrow, ncol = set_features["subtrain"].shape
 net = Net(ncol, 200, 100, 50, 10, 1)
 feature_tensor = torch.from_numpy(set_features["subtrain"]).float()
 net(feature_tensor).shape
@@ -94,3 +121,30 @@ loss_tensor
 label_neg_pos = np.where(label_01 == 1, 1, -1)
 np.mean(np.log(1+np.exp(-label_neg_pos * pred_vec.detach().numpy())))
 
+
+model = Net(ncol, 100, 10, 1)
+loss_fun = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+loss_df_list = []
+# gradient descent 
+MAX_EPOCHS = 10
+for epoch in range(MAX_EPOCHS):
+    for batch_features, batch_labels in dl:
+        optimizer.zero_grad()
+        pred_tensor = model(batch_features.float()).reshape(len(batch_labels))
+        loss_tensor = loss_fun(pred_tensor, batch_labels.float())
+        loss_tensor.backward()
+        optimizer.step()
+    for set_name in set_features:
+        set_X = set_features[set_name]
+        set_y = set_labels[set_name]
+        set_X_tensor = torch.from_numpy(set_X).float()
+        set_y_tensor = torch.from_numpy(set_y)
+        set_pred = model(set_X_tensor).reshape(len(set_y))
+        set_loss = loss_fun(set_pred, set_y_tensor.float())
+        loss_df_list.append(pd.DataFrame({
+            "set_name":set_name,
+            "loss":float(set_loss),
+            "epoch":epoch
+        }, index=[0]))
+loss_df =pd.concat(loss_df_list)
